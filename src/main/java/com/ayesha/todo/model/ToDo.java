ac1b3c96fa686e74bd67e99597cc53bccfd2b262
aayshachauhan <aayshachauhan23@gmail.com>
2023-12-12 12:38:22 +0530

    Base structure and database connectivity

---
+++ src/main/java/com/ayesha/todo/model/ToDo.java
@@ -0,0 +1,66 @@
+package com.ayesha.todo.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todo")
+public class ToDo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name ="id")
+	private Long id;
+
+	@Column(name ="title")
+	private String title;
+
+	@Column(name ="date")
+	private Date date;
+
+	@Column(name ="status")
+	private String status;
+
+	public ToDo() {
+
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
